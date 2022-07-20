0419f66c852235c56f123a63c49ef294a754b375
Trevor L Davis <trevor.l.davis@gmail.com>
2022-07-20 17:40:02 +0000

    version 1.7.3

--- inst/doc/optparse.R
+++ inst/doc/optparse.R
@@ -1,31 +1,30 @@
 ## ----echo=FALSE---------------------------------------------------------------
 library("knitr")
-Rscript_executable <- paste(file.path(R.home(), "bin", "Rscript"), "--vanilla")
-opts_knit$set(root.dir = system.file("exec", package="optparse")) # to access the "Rscript files"
-opts_chunk$set(comment=NA, echo=FALSE) 
-# setwd(system.file("exec", package="optparse")) # to access the "Rscript files"
+Rscript_executable <- paste(file.path(R.home(), "bin", "Rscript"), "--vanilla") # nolint
+opts_knit$set(root.dir = system.file("exec", package = "optparse")) # to access the "Rscript files"
+opts_chunk$set(comment = NA, echo = FALSE)
 list_file_command <- "ls"
 chmod_command <- "chmod ug+x display_file.R example.R"
 path_command <- "export PATH=$PATH:`pwd`"
-run_command <- function(string) { suppressWarnings(cat(system(string, intern=TRUE), sep="\n")) }
+run_command <- function(string) suppressWarnings(cat(system(string, intern = TRUE), sep = "\n"))
 
 ## -----------------------------------------------------------------------------
 run_command(sprintf("%s", list_file_command))
 command <- "display_file.R example.R" # to show file
 
 ## -----------------------------------------------------------------------------
-run_command(sprintf("%s %s 2>&1", Rscript_executable, command))   
+run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
 
 ## -----------------------------------------------------------------------------
 command <- "example.R --help" # same as system("Rscript example.R -h")
 
 ## -----------------------------------------------------------------------------
-run_command(sprintf("%s %s 2>&1", Rscript_executable, command))   
+run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
 command <- "example.R" # rely only on defaults
 
 ## -----------------------------------------------------------------------------
-run_command(sprintf("%s %s 2>&1", Rscript_executable, command))   
-command <- "example.R --mean=10 --sd=10 --count=3" 
+run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
+command <- "example.R --mean=10 --sd=10 --count=3"
 
 ## -----------------------------------------------------------------------------
 run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
@@ -43,15 +42,15 @@ command <- "example.R -c 100 -c 2 -c 1000 -c 7" #  same as above but "quiet"
 run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
 
 ## -----------------------------------------------------------------------------
-command <- "display_file.R --help" 
+command <- "display_file.R --help"
 
 ## -----------------------------------------------------------------------------
 run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
-command <- "display_file.R --add_numbers display_file.R" 
+command <- "display_file.R --add_numbers display_file.R"
 
 ## -----------------------------------------------------------------------------
 run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
-command <- "display_file.R non_existent_file.txt" 
+command <- "display_file.R non_existent_file.txt"
 
 ## -----------------------------------------------------------------------------
 run_command(sprintf("%s %s 2>&1", Rscript_executable, command))
